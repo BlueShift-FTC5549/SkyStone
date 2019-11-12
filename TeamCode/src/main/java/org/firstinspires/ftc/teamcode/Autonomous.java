485417dcd755d2d97b43e3b632661d844b6bce68
robo19 <jonesk22@ashevilleschool.org>
2019-11-12 16:37:55 -0500

    v 0.1.0
    Testing Encoder Drive and IMU turning.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -19,30 +19,19 @@ public class Autonomous {
         setTelemetryStatus("Initialized");
     }
 
-    @Override
     public void runOpMode() {
         int cubePosition; //1, 2, or 3. Corresponds left to right from the perspective of the lander
 
         initialize();
 
-        waitForStart();
-
         telemetry.clearAll();
-
-        //Free the lift hook from the lander
-        autoFourWheelDrive.encoderStrafe(-5.0, 10);
 
         //Turn the rest of the angle to be facing away from the lander
         autoFourWheelDrive.turn(45, 6);
 
-        telemetry.addData("Cube Position: ", cubePosition);
+        autoFourWheelDrive.encoderDrive(10,10);
 
-
-        autoFourWheelDrive.encoderStrafe(-15,10);
-        telemetry.addData("Status","Turning");
-
-        //Find the gold block
-        telemetry.addData("Status","Aligning");
+        setTelemetryStatus("Turning");
     }
 
     public void setTelemetryStatus(String status) {
